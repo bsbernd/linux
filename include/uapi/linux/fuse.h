6166689b563a37b937a97b0a293d4034d2bb27e5
Bernd Schubert <bschubert@ddn.com>
2022-10-01 13:55:19 +0200

    fuse-dev use uring io

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -951,12 +951,23 @@ struct fuse_notify_retrieve_in {
 };
 
 struct fuse_uring_cfg {
-	uint64_t	flags; /* possible compat flags, unused for now */
-	uint32_t	num_queues;
-	uint32_t	per_core_queue:1;
-	uint32_t	queue_depth;
-	uint32_t	padding1;
-	uint64_t 	padding[8]; /* reserve space for future additions */
+	/* possible compat flags, unused for now */
+	uint64_t	compat_flags;
+
+	/* flag to have a queue per cpu core */
+	uint64_t	per_core_queue:1;
+
+	/* number of queues */
+	uint16_t	num_queues;
+
+	/* number of entries per queue */
+	uint16_t	queue_depth;
+
+	/* for all queues and their requests */
+	uint32_t	mmap_req_size;
+
+	/* reserved space for future additions */
+	uint64_t	padding2[8];
 };
 
 /* Device ioctls: */
@@ -1040,6 +1051,109 @@ struct fuse_secctx {
 struct fuse_secctx_header {
 	uint32_t	size;
 	uint32_t	nr_secctx;
+};
+
+
+/**
+ * Size of the ring buffer header
+ */
+#define FUSE_RING_HEADER_BUF_SIZE 4096
+
+enum fuse_ring_req_cmd {
+	FUSE_RING_BUF_CMD_INVALID = 0,
+
+	/* return an iovec pointer */
+	FUSE_RING_BUF_CMD_IOVEC = 1,
+
+	/* report an error */
+	FUSE_RING_BUF_CMD_ERROR = 2,
+};
+
+/* XXX: Reduce size as much as possible and fit into the 80B ring cmd */
+struct fuse_uring_buf_req {
+
+	union {
+		/* The first 4K are command data */
+		char in_out_buf[FUSE_RING_HEADER_BUF_SIZE];
+
+		struct {
+			/* fields below are set by kernel on filling a request
+			 * and later also by userspace on replying to a request
+			 */
+
+			uint64_t flags;
+
+			/* enum fuse_ring_buf_cmd */
+			uint32_t cmd;
+
+			/* size of the data buffer,
+			 * XXX Could be calculated from FUSE_RING_HEADER_BUF_SIZE
+			 * and fuse_uring_cmd_req::req_buf_len - use for
+			 * sanity check
+			 */
+			uint32_t data_buf_size;
+
+			union {
+				/* FUSE_URING_REQ_FETCH */
+
+				/* FUSE_RING_BUF_CMD_IOVEC_PTR */
+				struct {
+					void *iovec;
+					int32_t count;
+					int32_t iov_flags;
+				};
+
+				/* FUSE_RING_BUF_CMD_ERROR */
+				struct {
+					uint32_t result; /* always negative */
+					uint32_t padding1;
+				};
+			};
+			uint32_t buf_size_used;
+			uint32_t padding2;
+
+
+			/* kernel fills in, reads out */
+			union {
+				struct fuse_in_header in;
+				struct fuse_out_header out;
+			};
+		};
+	};
+	char data_buf[];
+} __attribute__ ((aligned(8)));
+
+/**
+ * sqe commands to the kernel
+ */
+enum fuse_uring_cmd {
+	FUSE_URING_REQ_INVALID = 0,
+
+	/* submit sqe to kernel to get a request */
+	FUSE_URING_REQ_FETCH = 1,
+
+	/* commit result and fetch next request */
+	FUSE_URING_REQ_COMMIT_AND_FETCH = 2,
+};
+
+/**
+ * In the 80B command area of the SQE.
+ */
+struct fuse_uring_cmd_req {
+	/* queue the command is for (queue index) */
+	uint16_t q_id;
+
+	/* queue entry (array index) */
+	uint16_t tag;
+
+	/* Submit the userspace result for the fuse request */
+	uint32_t result;
+
+	/* pointer to struct fuse_uring_buf_req */
+	uint64_t req_buf;
+	uint32_t req_buf_len;
+
+	uint32_t padding;
 };
 
 #endif /* _LINUX_FUSE_H */
