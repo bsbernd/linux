767105641b540bf7d0e93e8418dc14b307a0cff1
Jingbo Xu <jefflexu@linux.alibaba.com>
2024-07-05 18:04:49 +0800

    fuse: make foffset alignment opt-in for optimum backend performance
    Sometimes the file offset alignment needs to be opt-in to achieve the
    optimum performance at the backend store.
    
    For example when ErasureCode [1] is used at the backend store, the
    optimum write performance is achieved when the WRITE request is aligned
    with the stripe size of ErasureCode.  Otherwise a non-aligned WRITE
    request needs to be split at the stripe size boundary.  It is quite
    costly to handle these split partial requests, as firstly the whole
    stripe to which the split partial request belongs needs to be read out,
    then overwrite the read stripe buffer with the request, and finally write
    the whole stripe back to the persistent storage.
    
    Thus the backend store can suffer severe performance degradation when
    WRITE requests can not fit into one stripe exactly.  The write performance
    can be 10x slower when the request is 256KB in size given 4MB stripe size.
    Also there can be 50% performance degradation in theory if the request
    is not stripe boundary aligned.
    
    Besides, the conveyed test indicates that, the non-alignment issue
    becomes more severe when decreasing fuse's max_ratio, maybe partly
    because the background writeback now is more likely to run parallelly
    with the dirtier.
    
    fuse's max_ratio	ratio of aligned WRITE requests
    ----------------	-------------------------------
    70			99.9%
    40			74%
    20			45%
    10			20%
    
    With the patched version, which makes the alignment constraint opt-in
    when constructing WRITE requests, the ratio of aligned WRITE requests
    increases to 98% (previously 20%) when fuse's max_ratio is 10.
    
    [1] https://lore.kernel.org/linux-fsdevel/20240124070512.52207-1-jefflexu@linux.alibaba.com/T/#m9bce469998ea6e4f911555c6f7be1e077ce3d8b4
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -229,6 +229,8 @@
  *    - FUSE_URING_IN_OUT_HEADER_SZ
  *    - FUSE_URING_OP_IN_OUT_SZ
  *    - enum fuse_uring_cmd
+ *  7.43
+ *  - add opt_alignment to fuse_init_out, add FUSE_ALIGN_PG_ORDER init flag
  */
 
 #ifndef _LINUX_FUSE_H
@@ -435,6 +437,8 @@ struct fuse_file_lock {
  *		    of the request ID indicates resend requests
  * FUSE_ALLOW_IDMAP: allow creation of idmapped mounts
  * FUSE_OVER_IO_URING: Indicate that client supports io-uring
+ * FUSE_ALIGN_PG_ORDER: page order (power of 2 exponent for number of pages) for
+ *			optimimal io-size alignment
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -477,6 +481,7 @@ struct fuse_file_lock {
 #define FUSE_PASSTHROUGH	(1ULL << 37)
 #define FUSE_NO_EXPORT_SUPPORT	(1ULL << 38)
 #define FUSE_HAS_RESEND		(1ULL << 39)
+#define FUSE_ALIGN_PG_ORDER	(1ULL << 40)
 
 /* Obsolete alias for FUSE_DIRECT_IO_ALLOW_MMAP */
 #define FUSE_DIRECT_IO_RELAX	FUSE_DIRECT_IO_ALLOW_MMAP
@@ -896,6 +901,9 @@ struct fuse_init_in {
 #define FUSE_COMPAT_INIT_OUT_SIZE 8
 #define FUSE_COMPAT_22_INIT_OUT_SIZE 24
 
+/*
+ * align_page_order: Number of pages for optimal IO, or a multiple of that
+ */
 struct fuse_init_out {
 	uint32_t	major;
 	uint32_t	minor;
@@ -909,7 +917,9 @@ struct fuse_init_out {
 	uint16_t	map_alignment;
 	uint32_t	flags2;
 	uint32_t	max_stack_depth;
-	uint32_t	unused[6];
+	uint8_t		align_page_order;
+	uint8_t		padding[3];
+	uint32_t	unused[5];
 };
 
 #define CUSE_INIT_INFO_MAX 4096
