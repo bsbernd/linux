d95dd7ce5f821811f95bbbf2f2049bf9d9bafc9b
Bernd Schubert <bschubert@ddn.com>
2025-06-04 19:32:39 +0200

    fuse: {io-uring} Allow reduced number of ring queues
    Now that queue selection for fuse requests supports missing
    queues we don't need to wait for full queue initialization.
    In fact, fuse-server (daemon) might want to run with a reduced
    number of queues, for example to reduce memory usage.
    This also simplifies startup - after first queue entry
    registration fuse-io-uring is ready to be used.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- fs/fuse/dev_uring.c
+++ fs/fuse/dev_uring.c
@@ -1050,31 +1050,6 @@ static int fuse_uring_commit_fetch(struct io_uring_cmd *cmd, int issue_flags,
 	return 0;
 }
 
-static bool is_ring_ready(struct fuse_ring *ring, int current_qid)
-{
-	int qid;
-	struct fuse_ring_queue *queue;
-	bool ready = true;
-
-	for (qid = 0; qid < ring->max_nr_queues && ready; qid++) {
-		if (current_qid == qid)
-			continue;
-
-		queue = ring->queues[qid];
-		if (!queue) {
-			ready = false;
-			break;
-		}
-
-		spin_lock(&queue->lock);
-		if (list_empty(&queue->ent_avail_queue))
-			ready = false;
-		spin_unlock(&queue->lock);
-	}
-
-	return ready;
-}
-
 static int fuse_uring_map_qid(int qid, const struct cpumask *mask)
 {
 	int nr_queues = cpumask_weight(mask);
@@ -1152,13 +1127,9 @@ static void fuse_uring_do_register(struct fuse_ring_ent *ent,
 		return;
 
 	if (!ring->ready) {
-		bool ready = is_ring_ready(ring, queue->qid);
-
-		if (ready) {
-			WRITE_ONCE(fiq->ops, &fuse_io_uring_ops);
-			WRITE_ONCE(ring->ready, true);
-			wake_up_all(&fc->blocked_waitq);
-		}
+		WRITE_ONCE(fiq->ops, &fuse_io_uring_ops);
+		WRITE_ONCE(ring->ready, true);
+		wake_up_all(&fc->blocked_waitq);
 	}
 }
 
